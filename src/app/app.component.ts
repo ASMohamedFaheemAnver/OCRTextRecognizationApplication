05106c38f65359e20c28624e50673601352163dc
ubunturifa <jstrfaheem065@gmail.com>
2019-12-03 22:54:41 +0530

    Loginpage,

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,18 +8,18 @@ import { createWorker } from 'tesseract.js';
 })
 export class AppComponent{
   constructor(){
-    const worker = createWorker({
-      logger: m => console.log(m)
-    });
+    // const worker = createWorker({
+    //   logger: m => console.log(m)
+    // });
 
     // Run this function as soon as this function defined
-    (async () => {
-      await worker.load();
-      await worker.loadLanguage('eng');
-      await worker.initialize('eng');
-      const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
-      console.log(text);
-      await worker.terminate();
-    })();
+    // (async () => {
+    //   await worker.load();
+    //   await worker.loadLanguage('eng');
+    //   await worker.initialize('eng');
+    //   const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
+    //   console.log(text);
+    //   await worker.terminate();
+    // })();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { LoginComponent } from './pages/login/login.component';
+import { TopNavComponent } from './shared/top-nav/top-nav.component';
+import { MatInputModule,
+  MatFormFieldModule
+} from '@angular/material';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    TopNavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
