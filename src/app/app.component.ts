7bb7e3816ad21e367d308b1579809c685053efed
ubunturifa <jstrfaheem065@gmail.com>
2019-12-03 16:39:15 +0530

    Tesseract js function defined,

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { createWorker } from 'tesseract.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ocrapplication';
+export class AppComponent{
+  constructor(){
+    const worker = createWorker({
+      logger: m => console.log(m)
+    });
+
+    // Run this function as soon as this function defined
+    (async () => {
+      await worker.load();
+      await worker.loadLanguage('eng');
+      await worker.initialize('eng');
+      const { data: { text } } = await worker.recognize('https://tesseract.projectnaptha.com/img/eng_bw.png');
+      console.log(text);
+      await worker.terminate();
+    })();
+  }
 }
