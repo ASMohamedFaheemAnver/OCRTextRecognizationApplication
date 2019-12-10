4ca534eb91065ecedda2e5ec24ef188fb44c92b7
ubunturifa <jstrfaheem065@gmail.com>
2019-12-10 13:39:28 +0530

    Login page connected to backend,

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+// import { HttpClient } from '@angular/common/http';
+import { UserService } from 'src/app/user.service';
 
 @Component({
   selector: 'app-login',
@@ -9,19 +10,29 @@ import { HttpClient } from '@angular/common/http';
 })
 export class LoginComponent {
 
-  constructor(private router : Router, private http : HttpClient){}
-  url = 'http://localhost:8080/users/';
-  userName = 'jstrfaheem065@gmail.com';
-  password = '*74362@?';
+  constructor(private router : Router, private httpService : UserService){}
+  // url = 'http://localhost:8080/users/';
+  userData : any = []
 
-  onLoginSubmit(userData : FormData){
-    this.http.get(this.url).subscribe(res=>{
-      console.log(res);
-    });
+  onLoginSubmit(userFormData : FormData){
+    // this.http.get(this.url).subscribe(res=>{
+    //   console.log(res);
+    // });
     // console.log(userData['email']);
     //if(this.userName==userData['email']&&this.password==userData['password']){
       //this.router.navigateByUrl('home-page');
     //}
+
+
+    this.httpService.getUsers().subscribe(userData=>{
+      this.userData = userData;
+      this.userData.forEach(user => {
+        if(userFormData['email']==user['username']&&
+          userFormData['password']==user['password']){
+            this.router.navigateByUrl('home-page');   
+        }
+      });
+    });
   }
 
 }
