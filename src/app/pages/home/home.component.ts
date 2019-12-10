bcd3804849aafcab7bc064f07a0aa24e13aa2d64
ubunturifa <jstrfaheem065@gmail.com>
2019-12-10 21:53:08 +0530

    Getting image into ui,

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,9 +9,22 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+  selectedFile : File
   ngOnInit() {
   }
+  imageUrl : any = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTx5c-8qgZT0BceT9dz8NRrZlwe3bAOW98CK5uCDg6O2SjDUOum&s";
 
   onUpload(){}
+  onFileChanged(event){
+    const reader = new FileReader();
+    reader.readAsDataURL(event.target.files[0]);
+    
+    reader.onload= ((res)=>{
+      this.imageUrl = reader.result;
+    });
+    // const uploaddata = new FormData();
+    // this.selectedFile = event.target.files[0];
+    // console.log(this.selectedFile);
+  }
 
 }
