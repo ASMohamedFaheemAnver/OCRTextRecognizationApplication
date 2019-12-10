2b137472e50d43be996859ae37b24e9b4e9b498f
ubunturifa <jstrfaheem065@gmail.com>
2019-12-10 14:01:08 +0530

    User registration,

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+  url = 'http://localhost:8080/users';
+  user = {
+    username: 'jstrfaheem065@gmail.com',
+    password: '*74362@?'
+  }
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
-
-  ngOnInit() {
+  onLoginSubmit(userData) {
+    this.user.username = userData['email'];
+    this.user.password = userData['password'];
+    this.http.post(this.url, this.user).subscribe();
   }
 
 }
