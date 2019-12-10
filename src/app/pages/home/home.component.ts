12cbcdff4a88a1c39d396271dcda5d3b96791852
ubunturifa <jstrfaheem065@gmail.com>
2019-12-10 22:02:23 +0530

    Text detecting function added,

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { createWorker } from 'tesseract.js';
 
 @Component({
   selector: 'app-home',
@@ -10,11 +11,11 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   selectedFile : File
+
+  imageText = '';
   ngOnInit() {
   }
   imageUrl : any = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTx5c-8qgZT0BceT9dz8NRrZlwe3bAOW98CK5uCDg6O2SjDUOum&s";
-
-  onUpload(){}
   onFileChanged(event){
     const reader = new FileReader();
     reader.readAsDataURL(event.target.files[0]);
@@ -25,6 +26,23 @@ export class HomeComponent implements OnInit {
     // const uploaddata = new FormData();
     // this.selectedFile = event.target.files[0];
     // console.log(this.selectedFile);
+  }
+
+  onUpload(){
+    const worker = createWorker({
+      logger: m => console.log(m)
+    });
+
+    // Run this function as soon as this function defined
+    (async () => {
+      await worker.load();
+      await worker.loadLanguage('eng');
+      await worker.initialize('eng');
+      const { data: { text } } = await worker.recognize(this.imageUrl);
+      this.imageText = text;
+      console.log(text);
+      await worker.terminate();
+    })();
   }
 
 }
