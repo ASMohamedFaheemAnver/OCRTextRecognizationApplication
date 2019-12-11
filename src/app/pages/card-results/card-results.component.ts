ee515fc028bd6881eea31505b4bf3b37ed2d07e0
ubunturifa <jstrfaheem065@gmail.com>
2019-12-11 10:28:09 +0530

    Image posted,

--- src/app/pages/card-results/card-results.component.ts
+++ src/app/pages/card-results/card-results.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-card-results',
@@ -6,8 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-results.component.css']
 })
 export class CardResultsComponent implements OnInit {
+  imageToShow : any
+  sanitizer : DomSanitizer
+  constructor(private http : HttpClient) { 
+    
+    http.get('http://localhost:8080/images/1').subscribe(
+      res=>{
+        // this.createImageFromBlob(res);
+        let blob = new Blob([res['picture'], 'image/png']);
+        let objectURL = URL.createObjectURL(blob);
+        this.imageToShow = this.sanitizer.bypassSecurityTrustUrl(objectURL);
+        
+      }
+    );
+  }
 
-  constructor() { }
+//   createImageFromBlob(image: Blob) {
+//     let reader = new FileReader();
+//     reader.addEventListener("load", () => {
+//        this.imageToShow = reader.result;
+//       console.log(this.imageToShow);
+//     }, false);
+ 
+//     if (image) {
+//        reader.readAsDataURL(image);
+//     }
+//  }
 
   ngOnInit() {
   }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { createWorker } from 'tesseract.js';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -8,7 +9,7 @@ import { createWorker } from 'tesseract.js';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
 
   selectedFile : File
 
@@ -16,33 +17,43 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
   imageUrl : any = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTx5c-8qgZT0BceT9dz8NRrZlwe3bAOW98CK5uCDg6O2SjDUOum&s";
+  
   onFileChanged(event){
     const reader = new FileReader();
     reader.readAsDataURL(event.target.files[0]);
     
     reader.onload= ((res)=>{
+      console.log(res);
       this.imageUrl = reader.result;
+      console.log(this.imageUrl);
+      const worker = createWorker({
+        //logger: m => console.log(m)
+      });
+  
+      // Run this function as soon as this function defined
+      (async () => {
+        await worker.load();
+        await worker.loadLanguage('eng');
+        await worker.initialize('eng');
+        const { data: { text } } = await worker.recognize(this.imageUrl);
+        this.imageText = text;
+        console.log(text);
+        await worker.terminate();
+      })();
     });
     // const uploaddata = new FormData();
-    // this.selectedFile = event.target.files[0];
+    this.selectedFile = event.target.files[0];
     // console.log(this.selectedFile);
   }
 
   onUpload(){
-    const worker = createWorker({
-      logger: m => console.log(m)
-    });
-
-    // Run this function as soon as this function defined
-    (async () => {
-      await worker.load();
-      await worker.loadLanguage('eng');
-      await worker.initialize('eng');
-      const { data: { text } } = await worker.recognize(this.imageUrl);
-      this.imageText = text;
-      console.log(text);
-      await worker.terminate();
-    })();
+    const uploaddata = new FormData();
+    uploaddata.append('myFile', this.selectedFile, this.selectedFile.name);
+    this.http.post('http://localhost:8080/images', uploaddata).subscribe(
+      (res)=>{
+        console.log(res);
+      }
+    );
   }
 
 }
