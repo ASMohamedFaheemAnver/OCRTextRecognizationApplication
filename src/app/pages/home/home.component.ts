48b7fdb0a97936f12d45844926cfaa5101c5de16
ubunturifa <jstrfaheem065@gmail.com>
2019-12-16 17:58:18 +0530

    UI, UX,

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,31 +9,35 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private http : HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  selectedFile : File
+  selectedFile: File
 
   imageText = '';
   isUploading = false;
+  isInvalid = false;
+  isFinished = false;
 
   ngOnInit() {
   }
-  imageUrl : any = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTx5c-8qgZT0BceT9dz8NRrZlwe3bAOW98CK5uCDg6O2SjDUOum&s";
-  
-  onFileChanged(event){
+  imageUrl: any = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTx5c-8qgZT0BceT9dz8NRrZlwe3bAOW98CK5uCDg6O2SjDUOum&s";
+
+  onFileChanged(event) {
     this.isUploading = true;
+    this.isFinished = false;
+    this.isInvalid = false;
     const reader = new FileReader();
-    console.log(event.target.files[0]);
+    // console.log(event.target.files[0]);
     reader.readAsDataURL(event.target.files[0]);
-    
-    reader.onload= (()=>{
+
+    reader.onload = (() => {
       // console.log(res);
       this.imageUrl = reader.result;
       // console.log(this.imageUrl);
       const worker = createWorker({
         logger: m => console.log(m)
       });
-  
+
       // Run this function as soon as this function defined
       (async () => {
         await worker.load();
@@ -51,17 +55,22 @@ export class HomeComponent implements OnInit {
     // console.log(this.selectedFile);
   }
 
-  onUpload(){
-    this.isUploading = true;
-    const uploaddata = new FormData();
-    uploaddata.append('image', this.selectedFile, this.selectedFile.name);
-    uploaddata.append('result', this.imageText);
-    this.http.post('http://localhost:3000/api/results', uploaddata).subscribe(
-      (res)=>{
-        console.log(res);
-        this.isUploading = false;
-      }
-    );
+  onUpload() {
+    if (!this.selectedFile) {
+      this.isInvalid = true;
+    } else {
+      const uploaddata = new FormData();
+      uploaddata.append('image', this.selectedFile, this.selectedFile.name);
+      uploaddata.append('result', this.imageText);
+      this.http.post('http://localhost:3000/api/results', uploaddata).subscribe(
+        (res) => {
+          console.log(res);
+          this.isUploading = false;
+          this.isFinished = true;
+        }
+      );
+    }
+
   }
 
 }
