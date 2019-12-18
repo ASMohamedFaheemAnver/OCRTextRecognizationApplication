3418de1e5a9d8a443e65e47e9fb9c48d5ad80a4d
ubunturifa <jstrfaheem065@gmail.com>
2019-12-18 15:31:34 +0530

    Conditional topnav,

--- src/app/shared/top-nav/top-nav.component.ts
+++ src/app/shared/top-nav/top-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-top-nav',
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class TopNavComponent implements OnInit {
 
   constructor() { }
-
+  @Input() isIn = false;
   ngOnInit() {
   }
 
